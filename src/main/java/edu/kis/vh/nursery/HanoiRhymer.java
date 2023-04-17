cc2a4bbe38168fc8e2f6460aac5410ecac99d48d
juliaa-75 <240818@edu.p.lodz.pl>
2023-04-17 14:51:24 +0200

    3.scalenie galezi format i master

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,29 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
+<<<<<<< HEAD
 	int totalRejected = 0;
+=======
+    int totalRejected = 0;
+>>>>>>> origin/format
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+<<<<<<< HEAD
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo())
 			totalRejected++;
 		else
 			super.countIn(in);
 	}
+=======
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
+>>>>>>> origin/format
 }
