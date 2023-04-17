8db4098c63097ea6d3779047ff8c89ee37d1ec8f
juliaa-75 <240818@edu.p.lodz.pl>
2023-04-17 15:11:33 +0200

    5.1 poprawa nazw pol klas

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,29 +2,16 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-<<<<<<< HEAD
-	int totalRejected = 0;
-=======
-    int totalRejected = 0;
->>>>>>> origin/format
+int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
-
-<<<<<<< HEAD
-	public void countIn(int in) {
-		if (!callCheck() && in > peekaboo())
-			totalRejected++;
-		else
-			super.countIn(in);
-	}
-=======
-    public void countIn(int in) {
-        if (!callCheck() && in > peekaboo())
-            totalRejected++;
-        else
-            super.countIn(in);
+    public int reportRejected() {
+        return totalRejected;
     }
->>>>>>> origin/format
+
+    public void countIn(int in) {
+    if (!callCheck() && in > peekaboo())
+            totalRejected++;
+            else
+                super.countIn(in);
+    }
 }
